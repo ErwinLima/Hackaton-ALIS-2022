5b0535310ecf0714502a8daf19c0a794260dc283
ErwinLima <ealiman@correo.url.edu.gt>
2022-11-05 14:28:45 -0600

    Commit inicial

---
+++ Hackaton-ALIS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+namespace Hackaton;
+
+public class Program
+{
+    public static void Main()
+    {
+                
+    }
+}
